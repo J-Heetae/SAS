a546437e5f20edc55bb35aea7ad828d68629b5d7
BeomKyu <74239436+BeomKyu@users.noreply.github.com>
2024-10-24 12:34:22 +0900

    Update 연속된 부분 수열의 합_beomkyu.py

--- 241024_연속된 부분 수열의 합/연속된 부분 수열의 합_beomkyu.py
+++ 241024_연속된 부분 수열의 합/연속된 부분 수열의 합_beomkyu.py
@@ -1,1 +1,15 @@
-a
+import math
+def solution(sequence, k):
+    answer = []
+    x = 0
+    list_sum = 0
+    min_length = math.inf
+    for y in range(len(sequence)):
+        list_sum += sequence[y]
+        while list_sum >= k:
+            if list_sum == k and min_length > y - x:
+                min_length = y - x
+                answer = [x, y]
+            list_sum -= sequence[x]
+            x += 1
+    return answer
